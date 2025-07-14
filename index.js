8a97e7112e9c369f82ff9f97865a91837ca095ae
EmmAI03 <emmyrahmania@gmail.com>
2025-07-14 16:05:25 +0700

    fix: add userRoutes and Router imports to index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+import userRoutes from './routers/user.js';
+import { Router } from "express";
 
 mongoose.connect('mongodb://127.0.0.1:27017/express-test')
     .then(()=>{
